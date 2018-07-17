770efcf6afb8a10030e254a5c1a591edf1a77edd
Alexander Engelhardt <engelhardt@alpha-epsilon.de>
2018-07-17 21:46:46 +0200

    updated RetirementSim

--- RetirementSimulation/server.R
+++ RetirementSimulation/server.R
@@ -1,11 +1,12 @@
 library(shiny)
 library(tidyverse)
+library(scales)  # for labels=comma in ggplot axis
 
 source("functions.R")
 
 shinyServer(function(input, output) {
   
-  res <- eventReactive(input$go, {
+  res <- reactive({
     capital_growth(input)
   })
   
@@ -15,7 +16,9 @@ shinyServer(function(input, output) {
     colnames(to_plot) <- c("year", "n_sim", "money")
     
     ggplot(to_plot) + geom_line(aes(x=year, y=money, group=n_sim), alpha=0.12) +
-      ggtitle("MCMC Simulations")
+      scale_x_continuous("Jahr") +
+      scale_y_continuous("Vermögen", labels=comma) +
+      ggtitle("MCMC Simulationen")
   })
   
   output$final_hist <- renderPlot({
@@ -23,7 +26,9 @@ shinyServer(function(input, output) {
     final_fortune <- data.frame(fortune=as.numeric(result[input$Betrachtungszeitraum, 1:input$n_sim]))
     
     ggplot(final_fortune) + geom_histogram(aes(x=fortune), bins=30) +
-      ggtitle("Inflation-corrected final fortune")
+      scale_x_continuous("Vermögen", labels=comma) +
+      scale_y_continuous("Anzahl") + 
+      ggtitle("Inflationsbereinigtes Endvermögen")
   })
   
 })
--- RetirementSimulation/ui.R
+++ RetirementSimulation/ui.R
@@ -3,11 +3,12 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   headerPanel("Kapitalentwicklung"),
-  
+
   sidebarPanel(
+    tags$p("Konfigurieren Sie die Parameter, um interaktive MCMC-Simulationen zu erhalten."),
     h3("Misc"),
     sliderInput("Betrachtungszeitraum", "Betrachtungszeitraum in Jahren", min=1, max=100, value=30, step=1),
-    sliderInput("n_sim", "Anzahl der Simulationen", min=10, max=1000, value=200, step=10),
+    sliderInput("n_sim", "Anzahl der Simulationen", min=10, max=200, value=100, step=5),
     h3("Wirtschaft"),
     sliderInput("inflation", label = "jährl. Inflation in %", min = 0, max = 10, value = 1.8, step=0.1),
     sliderInput("vol_inflation", label = "Volatilität der Inflation in %", min = 0, max = 10, value = 1.5, step=0.1),
@@ -16,13 +17,16 @@ shinyUI(pageWithSidebar(
     h3("Sparen"),
     sliderInput("K0", label = "Startkapital in k€", min = 0, max = 1000, value = 10, step=10),
     sliderInput("monthly_savings", label = "monatlicher Sparbetrag", min=0, max=5000, value = 200, step=100),
-    sliderInput("monthly_savings_increase", label = "jährlicher Anstieg des Sparbetrags in Prozent", min=0, max=20, value = 3, step=0.25),
-    actionButton("go", "Go!", icon=icon("refresh"))
+    sliderInput("monthly_savings_increase", label = "jährlicher Anstieg des Sparbetrags in Prozent", min=0, max=20, value = 3, step=0.25)
+    # actionButton("go", "Go!", icon=icon("refresh"))
   ),
   
   mainPanel(
     plotOutput("cap_plot"),
-    plotOutput("final_hist")
+    tags$br(),
+    tags$hr(),
+    tags$br(),
+    plotOutput("final_hist")	
   )
   
 ))
